458bd33f893b7e3c4ce46f77707a29c7b2d80c33
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2016-10-20 10:20:01 +0200

    Enhancement/create docker configuration (#2)
    * [Create docker] WIP: added Dockerfile for django with entrypoint and start-dev scripts.
    
    * [Create docker] Fixes #1: add proper docker configuration
    
    * [Create docker] Small fixes

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,3 +11,6 @@ TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # Note: This key only used for development and testing.
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='CHANGEME!!!')
+
+# LOCALY DISABLE PASSWORDS CHECK
+AUTH_PASSWORD_VALIDATORS = []
