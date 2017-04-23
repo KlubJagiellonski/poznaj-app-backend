78c37dea32a654534fbe7b0ee61cde13f0166f74
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-04-23 14:30:28 +0200

    Enhancement/add aws account
    * Enable AWS in production
    
    * Bump version, fix tuple problem
    
    * Add region to s3
    
    * Fix errors in URLs
    
    * Update conf.py
    
    * Revert back to default values

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,7 @@
 import logging
 
+from boto.s3.connection import OrdinaryCallingFormat
+
 from django.utils import six
 
 from .common import *  # noqa
@@ -55,8 +57,9 @@ INSTALLED_APPS += ('storages', )
 AWS_ACCESS_KEY_ID = env('DJANGO_AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
-AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_AUTO_CREATE_BUCKET = True
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()
 
 
 # AWS cache settings, don't change unless you know what you're doing:
